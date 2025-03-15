3940a0f119a0fdc4fde1d9ead3b47d864befd2a7
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-15 17:26:29 +0100

    New pages and routes added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import GamesView from '@/views/GamesView.vue'
+import AdminView from '@/views/AdminView.vue'
+import AuthView from '@/views/AuthView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +12,21 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/games',
+      name: 'games',
+      component: GamesView,
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView
+    }
   ],
 })
 
