d12f1631b9b729186afcce9245eb303d020daa05
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-15 15:24:22 +0100

    fetchGames function added

--- src/modules/useGames.ts
+++ src/modules/useGames.ts
@@ -5,4 +5,28 @@ export default function useGames() {
    const error = ref<string | null>(null);
    const loading = ref<boolean>(false);
    const games = ref<Game[]>([]);
+
+   const fetchGames = async (): Promise<void> => {
+      loading.value = true;
+
+      try {
+         const response = await fetch(`${import.meta.env.API_BASE_URL}/api/games`);
+
+         if (!response.ok) {
+            throw new Error('No data available!');
+         }
+
+         const data: Game[] = await response.json();
+         games.value = data;
+         console.log("Games fetched: ", games.value);
+      }
+      catch (err) {
+         error.value = (err as Error).message;
+      }
+      finally {
+         loading.value = false;
+      }
+   }
+
+   return { error, loading, games, fetchGames };
 }
