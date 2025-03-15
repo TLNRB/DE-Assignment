6046f7e66ce57ceeb687f63f9855680f6ecec52f
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-15 10:55:23 +0100

    tailwindcss added, project cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1 class="">Home</h1>
+  </div>
 </template>
+
+<style scoped></style>
