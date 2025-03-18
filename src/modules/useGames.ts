564d67aa8d7b38d42dfeeab0334069de4a3e77bf
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-18 19:57:13 +0100

    refactor of useGames functions

--- src/modules/useGames.ts
+++ src/modules/useGames.ts
@@ -28,17 +28,41 @@ export const useGames = () => {
       }
    }
 
-   const addGame = async (newGame: NewGame): Promise<void> => {
-      try {
-         const token: string | null = localStorage.getItem('token');
-         const userId: string | null = localStorage.getItem('userId');
+   const getTokenAndUserId = (): { token: string, userId: string } => {
+      const token: string | null = localStorage.getItem('token');
+      const userId: string | null = localStorage.getItem('userId');
 
-         if (!token) {
-            throw new Error('Not authenticated!');
-         }
-         if (!userId) {
-            throw new Error('No user id available!');
-         }
+      if (!token) throw new Error('Not authenticated!');
+      if (!userId) throw new Error('No user id available!');
+
+      return { token, userId };
+   }
+
+   const convertPlatformsArrayToString = (game: NewGame, platforms: string[]): void => {
+      game.platform = platforms.join(', ');
+   }
+
+   const setDefaultValues = (game: NewGame, platforms: string[], userId: string): NewGame => {
+      convertPlatformsArrayToString(game, platforms);
+
+      return {
+         title: game.title,
+         description: game.description,
+         imageURL: 'https://picsum.photos/500/500',
+         price: game.price,
+         rating: game.rating,
+         platform: game.platform,
+         genre: game.genre,
+         releaseDate: game.releaseDate,
+         _createdBy: userId,
+      }
+   }
+
+   const addGame = async (game: NewGame, platforms: string[]): Promise<void> => {
+      try {
+         const { token, userId } = getTokenAndUserId();
+
+         const newGame: NewGame = setDefaultValues(game, platforms, userId);
 
          const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/games`, {
             method: 'POST',
@@ -50,7 +74,8 @@ export const useGames = () => {
          });
 
          if (!response.ok) {
-            throw new Error('Failed to add game!');
+            const errorData = await response.json();
+            throw new Error(`Failed to add game! ${errorData.error}`);
          }
          else {
             const game: Game = await response.json();
@@ -81,7 +106,8 @@ export const useGames = () => {
             });
 
             if (!response.ok) {
-               throw new Error('Failed to delete game!');
+               const errorData = await response.json();
+               throw new Error(`Failed to delete game! ${errorData.error}`);
             }
             else {
                games.value = games.value.filter(game => game._id !== id);
@@ -94,5 +120,5 @@ export const useGames = () => {
       }
    }
 
-   return { error, loading, games, fetchGames, addGame, deleteGame };
+   return { error, loading, games, fetchGames, addGame, deleteGame, getTokenAndUserId };
 }
