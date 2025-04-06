6530d8162e0bf4fb6b9f7b62cdcb4c3e61e92d5d
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-06 10:52:56 +0200

    input field naming

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -20,8 +20,7 @@ const handleLogin = async (email: string, password: string): Promise<void> => {
 
 <template>
   <main
-    class="w-full min-h-[calc(100vh-65px)] py-[2rem] px-[1rem] lg:px-[2rem] flex flex-col justify-center items-center gap-[1rem]"
-  >
+    class="w-full min-h-[calc(100vh-65px)] py-[2rem] px-[1rem] lg:px-[2rem] flex flex-col justify-center items-center gap-[1rem]">
     <div class="card w-96 bg-base-100 shadow-md p-6 mt-[-65px]">
       <h1 class="text-2xl font-bold text-center mb-4">Login</h1>
       <form class="space-y-4" @submit.prevent="handleLogin(email, password)">
@@ -30,13 +29,8 @@ const handleLogin = async (email: string, password: string): Promise<void> => {
           <label class="label">
             <span class="label-text">Email</span>
           </label>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            class="input input-bordered w-full"
-            required
-            v-model.trim="email"
-          />
+          <input type="email" name="email" placeholder="Enter your email" class="input input-bordered w-full" required
+            v-model.trim="email" />
         </div>
 
         <!-- Password Field -->
@@ -44,13 +38,8 @@ const handleLogin = async (email: string, password: string): Promise<void> => {
           <label class="label">
             <span class="label-text">Password</span>
           </label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            class="input input-bordered w-full"
-            required
-            v-model="password"
-          />
+          <input type="password" name="password" placeholder="Enter your password" class="input input-bordered w-full"
+            required v-model="password" />
         </div>
         <button type="submit" class="btn btn-primary w-full">Login</button>
       </form>
@@ -68,18 +57,9 @@ const handleLogin = async (email: string, password: string): Promise<void> => {
 
     <!-- Error display -->
     <div v-else-if="error" role="alert" class="alert alert-error alert-soft w-96">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 shrink-0 stroke-current"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       <span>{{ error }}</span>
     </div>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -20,8 +20,7 @@ const handleRegister = async (name: string, email: string, password: string): Pr
 
 <template>
   <main
-    class="w-full min-h-[calc(100vh-65px)] py-[2rem] px-[1rem] lg:px-[2rem] flex flex-col justify-center items-center gap-[1rem]"
-  >
+    class="w-full min-h-[calc(100vh-65px)] py-[2rem] px-[1rem] lg:px-[2rem] flex flex-col justify-center items-center gap-[1rem]">
     <div class="card w-96 bg-base-100 shadow-md p-6 mt-[-65px]">
       <h1 class="text-2xl font-bold text-center mb-4">Register</h1>
       <form class="space-y-4" @submit.prevent="handleRegister(name, email, password)">
@@ -30,13 +29,8 @@ const handleRegister = async (name: string, email: string, password: string): Pr
           <label class="label">
             <span class="label-text">Name</span>
           </label>
-          <input
-            type="text"
-            placeholder="Enter your name"
-            class="input input-bordered w-full"
-            required
-            v-model="name"
-          />
+          <input type="text" name="name" placeholder="Enter your name" class="input input-bordered w-full" required
+            v-model="name" />
         </div>
 
         <!-- Email Field -->
@@ -44,13 +38,8 @@ const handleRegister = async (name: string, email: string, password: string): Pr
           <label class="label">
             <span class="label-text">Email</span>
           </label>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            class="input input-bordered w-full"
-            required
-            v-model.trim="email"
-          />
+          <input type="email" name="email" placeholder="Enter your email" class="input input-bordered w-full" required
+            v-model.trim="email" />
         </div>
 
         <!-- Password Field -->
@@ -58,13 +47,8 @@ const handleRegister = async (name: string, email: string, password: string): Pr
           <label class="label">
             <span class="label-text">Password</span>
           </label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            class="input input-bordered w-full"
-            required
-            v-model="password"
-          />
+          <input type="password" name="password" placeholder="Enter your password" class="input input-bordered w-full"
+            required v-model="password" />
         </div>
         <button type="submit" class="btn btn-primary w-full">Register</button>
       </form>
@@ -82,18 +66,9 @@ const handleRegister = async (name: string, email: string, password: string): Pr
 
     <!-- Error display -->
     <div v-else-if="error" role="alert" class="alert alert-error alert-soft w-96">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 shrink-0 stroke-current"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       <span>{{ error }}</span>
     </div>
